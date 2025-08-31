1fcdbc4dc115d86327357251701278dfb1836cde
Raí Vandenberg <rvdesigner20@gmail.com>
2025-08-31 17:22:42 -0300

    feat add Clientes Model e create one entity in db

---
+++ src/main/java/dev/vandenberg/CadastroDeClientes/ClientesModel.java
@@ -0,0 +1,49 @@
+package dev.vandenberg.CadastroDeClientes;
+
+import jakarta.persistence.*;
+
+//Entity eke transforma uma classe em uma entidade do bd
+@Entity
+@Table(name = "tb_cadastro")
+public class ClientesModel {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+   private Long id;
+   private String nome;
+   private String email;
+   private int idade;
+
+    public ClientesModel() {
+    }
+
+    public ClientesModel(String nome, String email, int idade) {
+        this.nome = nome;
+        this.email = email;
+        this.idade = idade;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public int getIdade() {
+        return idade;
+    }
+
+    public void setIdade(int idade) {
+        this.idade = idade;
+    }
+}

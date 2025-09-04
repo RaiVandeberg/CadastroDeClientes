ec9d1cb4b9f10b935e5b2a411bbec47eb1983033
Raí Vandenberg <rvdesigner20@gmail.com>
2025-09-03 21:55:24 -0300

    feat: add relation between columns DependenteModel and ClienteModel
    # Conflicts:
    #	src/main/java/dev/vandenberg/CadastroDeClientes/Dependentes/DependenteModel.java

--- src/main/java/dev/vandenberg/CadastroDeClientes/Dependentes/DependenteModel.java
+++ src/main/java/dev/vandenberg/CadastroDeClientes/Dependentes/DependenteModel.java
@@ -15,59 +15,7 @@ public class DependenteModel {
     private String telefone;
     private String dataNascimento;
 
-    //
-    @ManyToOne
-    @JoinColumn(name = "cliente_id" )
-    private ClienteModel clientes;
+    private ClienteModel cliente;
 
 
-
-    public DependenteModel(String dataNascimento, String telefone, String email, String nome, Long id) {
-
-        this.dataNascimento = dataNascimento;
-        this.telefone = telefone;
-        this.email = email;
-        this.nome = nome;
-        this.id = id;
-    }
-
-    public Long getId() {
-        return id;
-    }
-
-    public void setId(Long id) {
-        this.id = id;
-    }
-
-    public String getNome() {
-        return nome;
-    }
-
-    public void setNome(String nome) {
-        this.nome = nome;
-    }
-
-    public String getEmail() {
-        return email;
-    }
-
-    public void setEmail(String email) {
-        this.email = email;
-    }
-
-    public String getTelefone() {
-        return telefone;
-    }
-
-    public void setTelefone(String telefone) {
-        this.telefone = telefone;
-    }
-
-    public String getDataNascimento() {
-        return dataNascimento;
-    }
-
-    public void setDataNascimento(String dataNascimento) {
-        this.dataNascimento = dataNascimento;
-    }
 }

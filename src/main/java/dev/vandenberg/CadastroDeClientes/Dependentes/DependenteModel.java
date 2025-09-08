14a0ef4501774ee6860484b537f30e34aaebbec3
Raí Vandenberg <rvdesigner20@gmail.com>
2025-09-07 21:11:38 -0300

    merge lombok

--- src/main/java/dev/vandenberg/CadastroDeClientes/Dependentes/DependenteModel.java
+++ src/main/java/dev/vandenberg/CadastroDeClientes/Dependentes/DependenteModel.java
@@ -3,9 +3,15 @@ package dev.vandenberg.CadastroDeClientes.Dependentes;
 
 import dev.vandenberg.CadastroDeCliente.Cliente.ClienteModel;
 import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
 
 @Entity
 @Table(name = "tb_dependente")
+@NoArgsConstructor
+@AllArgsConstructor
+@Data
 public class DependenteModel {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
@@ -20,57 +26,4 @@ public class DependenteModel {
     @JoinColumn(name = "cliente_id" )
     private ClienteModel clientes;
 
-
-    public DependenteModel() {
-
-    }
-
-    public DependenteModel(String dataNascimento, String telefone, String email, String nome, Long id) {
-
-        this.dataNascimento = dataNascimento;
-        this.telefone = telefone;
-        this.email = email;
-        this.nome = nome;
-        this.id = id;
-    }
-
-    public Long getId() {
-        return id;
-    }
-
-    public void setId(Long id) {
-        this.id = id;
-    }
-
-    public String getNome() {
-        return nome;
-    }
-
-    public void setNome(String nome) {
-        this.nome = nome;
-    }
-
-    public String getEmail() {
-        return email;
-    }
-
-    public void setEmail(String email) {
-        this.email = email;
-    }
-
-    public String getTelefone() {
-        return telefone;
-    }
-
-    public void setTelefone(String telefone) {
-        this.telefone = telefone;
-    }
-
-    public String getDataNascimento() {
-        return dataNascimento;
-    }
-
-    public void setDataNascimento(String dataNascimento) {
-        this.dataNascimento = dataNascimento;
-    }
 }

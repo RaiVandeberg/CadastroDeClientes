7bdf952223d7eb74a437279617e5f9ddad90af22
Raí Vandenberg <rvdesigner20@gmail.com>
2025-09-18 23:27:25 -0300

    feat add router in DependenteController

--- src/main/java/dev/vandenberg/Dependentes/DependenteController.java
+++ src/main/java/dev/vandenberg/Dependentes/DependenteController.java
@@ -1,14 +1,32 @@
 package dev.vandenberg.Dependentes;
 
 
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
-@RequestMapping
+@RequestMapping("dependentes")
 public class DependenteController {
 
-    @GetMapping("/dependentes")
+    @PostMapping("/criar")
     public String dependentes (){ return "Dependentes";}
+
+    @GetMapping("/all")
+    public String listarDependentes(){
+        return "Dependentes";
+    }
+
+    @PutMapping("/id")
+    public  String atualizarDependentePorId(){
+        return "Atualizar dependente";
+    }
+
+    @GetMapping("/id")
+    public  String mostrarDependentePorId(){
+        return "Mostrar dependente";
+    }
+
+    @DeleteMapping("/id")
+    public  String deletarDependentePorId(){
+        return "Deletar dependente";
+    }
 }

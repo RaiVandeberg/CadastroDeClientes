557ed6286214c01421a09ff25d45834d32fb7b99
Raí Vandenberg <rvdesigner20@gmail.com>
2025-09-18 23:05:15 -0300

    feat update controller with mock data

--- src/main/java/dev/vandenberg/Clients/ClienteController.java
+++ src/main/java/dev/vandenberg/Clients/ClienteController.java
@@ -1,8 +1,6 @@
 package dev.vandenberg.Clients;
 
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping
@@ -12,5 +10,31 @@ public class ClienteController {
     public String boasVindas (){
         return "Boas vindas";
     }
+
+    // adicionar cliente
+    @PostMapping("/criar")
+    public String criarCliente(){
+        return  "Cliente criado";
+    }
+
+    @GetMapping("/all")
+    public String mostrarCliente(){
+        return  "Mostrar cliente";
+    }
+
+   @GetMapping("/id")
+   public String mostrarClienteId(){
+       return  "Mostrar cliente por id";
+   }
+
+   @PutMapping("/id")
+    public  String atualizarClientePorId(){
+        return  "Cliente atualizado";
+   }
+
+   @DeleteMapping("/id")
+    public String deletarClientePorId(){
+        return  "Cliente deletado";
+   }
 
 }

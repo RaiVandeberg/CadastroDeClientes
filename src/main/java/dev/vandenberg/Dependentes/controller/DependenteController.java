5067922231e8cc3ba7c74e212a7db371a0eee864
Rai Vandenberg <rvdesigner20@gmail.com>
2025-10-14 20:54:16 -0300

    refactor: refatora o controller para incluir o responseEntity nas minhas rotas e customizar as respostas

--- src/main/java/dev/vandenberg/Dependentes/controller/DependenteController.java
+++ src/main/java/dev/vandenberg/Dependentes/controller/DependenteController.java
@@ -4,6 +4,8 @@ package dev.vandenberg.Dependentes.controller;
 import dev.vandenberg.Dependentes.DependenteDTO;
 import dev.vandenberg.Dependentes.DependenteModel;
 import dev.vandenberg.Dependentes.services.DependenteService;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
@@ -19,28 +21,53 @@ public class DependenteController {
     }
 
     @PostMapping
-    public DependenteDTO criarDependente(@RequestBody DependenteDTO dependente) {
-        return dependenteService.criarDependente(dependente);
+    public ResponseEntity<String> criarDependente(@RequestBody DependenteDTO dependente) {
+        DependenteDTO dependenteDTO = dependenteService.criarDependente(dependente);
+        return ResponseEntity.status(HttpStatus.CREATED)
+                .body("dependente cadastrado com sucesso: " + dependenteDTO.getNome() + " (ID: " + dependenteDTO.getId() + ")");
     }
 
 
     @GetMapping("/all")
-    public List<DependenteDTO> listarDependentes(){
-        return dependenteService.listarDependentes();
+    public ResponseEntity<List<DependenteDTO>> listarDependentes(){
+
+        List <DependenteDTO> dependentes = dependenteService.listarDependentes();
+        return ResponseEntity.ok(dependentes);
     }
+
 
     @PutMapping("/{id}")
-    public DependenteDTO atualizarDependente(@PathVariable Long id, @RequestBody DependenteDTO dependente) {
-        return dependenteService.atualizarDependente(id, dependente);
+    public ResponseEntity<?> atualizarDependente(@PathVariable Long id, @RequestBody DependenteDTO dependente) {
+       DependenteDTO dependenteDTO = dependenteService.atualizarDependente(id, dependente);
+
+       if(dependenteDTO != null){
+           return ResponseEntity.ok(dependenteDTO);
+       } else  {
+           return ResponseEntity.status(HttpStatus.NOT_FOUND).body("dependete com o id " + id + " nao encontrado");
+       }
     }
 
+
     @GetMapping("/{id}")
-    public  DependenteDTO buscarDependentePorId(@PathVariable Long id){
-        return dependenteService.buscarDependentePorId(id);
+    public  ResponseEntity<?> buscarDependentePorId(@PathVariable Long id) {
+        DependenteDTO dependenteDTO =  dependenteService.buscarDependentePorId(id);
+
+        if( dependenteDTO != null){
+            return ResponseEntity.ok(dependenteDTO);
+        } else {
+            return ResponseEntity.status(HttpStatus.NOT_FOUND).body(" depende com o id " + id + " nao existe");
+        }
     }
 
     @DeleteMapping("/{id}")
-    public void deletarDependentePorId(@PathVariable Long id){
+    public ResponseEntity<String> deletarDependentePorId(@PathVariable Long id){
+
+        if(dependenteService.buscarDependentePorId(id) != null){
          dependenteService.deletarDependentePorId(id);
+         return ResponseEntity.ok(" Dependente removido com sucesso! id: " + id);
+        } else {
+            return ResponseEntity.status(HttpStatus.NOT_FOUND)
+                    .body(" dependente nao encontrado " + id);
+        }
     }
 }
